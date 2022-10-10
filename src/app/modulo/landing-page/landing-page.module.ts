ff9043733f14addad91f9c3fdc7b196e534b3db9
Eduardo Rocha <eduardo_rocha87@hotmail.com>
2022-10-09 21:07:57 -0300

    feat(rodape): criação do componente

--- src/app/modulo/landing-page/landing-page.module.ts
+++ src/app/modulo/landing-page/landing-page.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeadingComponent } from './heading/heading.component';
-import { FooterComponent } from './footer/footer.component';
-import { ContentComponent } from './content/content.component';
 import { LandingPageComponent } from './views/landing-page.component';
 import { landingPageRoutingModule } from './landing-page-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { ListNewsComponent } from './list-news/list-news.component';
-import { MaisLidasComponent } from './mais-lidas/mais-lidas.component';
+import { HeadingComponent } from './componentes/heading/heading.component';
+import { ContentComponent } from './componentes/content/content.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ListNewsComponent } from './componentes/list-news/list-news.component';
+import { MaisLidasComponent } from './componentes/mais-lidas/mais-lidas.component';
 
 
 
